826c2bffddaa03648805f279a2cca4e606315a35
EverEmpty <fairel95@gmail.com>
2023-06-04 01:00:28 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String url = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String username = "root";
+    private static final String password = "dsmbr";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
